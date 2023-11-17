68e20e541826217e0523e9be03f3012336349542
AdityaPhadnis1 <adityaphadnis606@gmail.com>
2023-11-17 22:30:26 +0530

    Test2 class

---
+++ src/secondproject/Test2.java
@@ -0,0 +1,25 @@
+package secondproject;
+
+public class Test2 {
+	public static void main(String args[]) {
+		
+		int x;
+		x=100;
+		char c;
+		c='j';
+		boolean b;
+		b=true;
+		double d;
+		d=20.16;
+		String s;
+		s="common";
+		
+		System.out.println(x);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(d);
+		System.out.println(s);
+		
+	}
+
+}
