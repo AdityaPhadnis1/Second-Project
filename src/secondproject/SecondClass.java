e2b209dc0990df7e2e6de185837c9ab691a416d6
AdityaPhadnis1 <adityaphadnis606@gmail.com>
2023-11-17 21:30:43 +0530

    This is My Second Project Commit

---
+++ src/secondproject/SecondClass.java
@@ -0,0 +1,16 @@
+package secondproject;
+
+public class SecondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is java program");
+		System.out.println("Java is a Programming Language");
+		System.out.println("java is a object oriented language");
+		System.out.println("Java is a high Level Language");
+		System.out.println("Java is a platform independent");
+
+	}
+
+}
